5be385c057ffe31a2a39b42d0ecd998d03b8de5a
majean5 <Mathilde.JEAN3@etu.uca.fr>
2022-12-13 15:58:15 +0100

    changement validation

--- config/Validation.php
+++ config/Validation.php
@@ -13,7 +13,7 @@
         }
 
         static function clear_string($champ){
-            // A changer filter_var
+            return filter_var($champ, FILTER_SANITIZE_SPECIAL_VAR);
         }
     }
 
